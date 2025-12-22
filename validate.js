feeee695a7063feeb68833f8e0a566d52cb6bae1
Tom Nielsen <tanielsen@gmail.com>
2025-12-22 18:43:15 +0000

    validate wip

--- validate.js
+++ validate.js
@@ -7,8 +7,17 @@ module.exports = {
       viewtemplate: "Available Resources Feed",
       table_id: table.id,
     });
-    const tables = Table.find({ provider_name: "Reservation availabilites" });
+    const tables = await Table.find(
+      {
+        provider_name: "Reservation availabilites",
+      },
+      {
+        cached: true,
+      }
+    );
     const neither = views.length === 0 && tables.length === 0;
+    //console.log({ views, tables, neither });
+
     return [
       ...(views.length || neither
         ? [
@@ -61,43 +70,59 @@ module.exports = {
       slots_available_field,
       start_field,
       end_field,
+      duration_field,
     } = get_config();
     //get all relevant reservations
 
-    const ress = await table.getRows({
-      [reservable_entity_key]:
-        row[reservable_entity_key]?.id || row[reservable_entity_key],
-      [start_field]: { lt: row[end_field], equal: true, day_only: true },
-      [end_field]: { gt: row[start_field], equal: true, day_only: true },
-      ...(valid_field ? { [valid_field]: true } : {}),
-    });
+    const q = valid_field ? { [valid_field]: true } : {};
+    if (end_field) {
+      q[start_field] = { lt: row[end_field], equal: true, day_only: true };
+      q[end_field] = { gt: row[start_field], equal: true, day_only: true };
+    } else {
+      q[start_field] = {
+        lt: row[start_field],
+        gt: row[start_field],
+        equal: true,
+        day_only: true,
+      };
+    }
+    if (reservable_entity_key)
+      q[reservable_entity_key] =
+        row[reservable_entity_key]?.id || row[reservable_entity_key];
+    //console.log("q", q);
+
+    const ress = await table.getRows(q);
 
     //get entity
-    const refield = table.getField(reservable_entity_key);
-    const retable = Table.findOne(refield.reftable_name);
-    const entity = await retable.getRow({
-      [retable.pk_name]:
-        row[reservable_entity_key]?.id || row[reservable_entity_key],
-    });
-    //check that for every day, there is availablity
-    const from = new Date(row[start_field]);
-    const to = new Date(row[end_field]);
-    let maxAvailable = entity[slots_available_field];
-    // loop for every day
-    for (let day = from; day <= to; day.setDate(day.getDate() + 1)) {
-      // your day is here
-      const active = ress.filter(
-        (r) => r[start_field] <= day && r[end_field] >= day
-      );
-      const taken = active
-        .map((r) => r[slot_count_field])
-        .reduce((a, b) => a + b, 0);
-      maxAvailable = Math.min(
-        maxAvailable,
-        entity[slots_available_field] - taken
-      );
+    if (reservable_entity_key && end_field) {
+      const refield = table.getField(reservable_entity_key);
+      const retable = Table.findOne(refield.reftable_name);
+      const entity = await retable.getRow({
+        [retable.pk_name]:
+          row[reservable_entity_key]?.id || row[reservable_entity_key],
+      });
+      //check that for every day, there is availablity
+      const from = new Date(row[start_field]);
+      const to = new Date(row[end_field]);
+      let maxAvailable = entity[slots_available_field];
+      // loop for every day
+      for (let day = from; day <= to; day.setDate(day.getDate() + 1)) {
+        // your day is here
+        const active = ress.filter(
+          (r) => r[start_field] <= day && r[end_field] >= day
+        );
+        const taken = active
+          .map((r) => r[slot_count_field])
+          .reduce((a, b) => a + b, 0);
+        maxAvailable = Math.min(
+          maxAvailable,
+          entity[slots_available_field] - taken
+        );
+      }
+      if (maxAvailable < row[slot_count_field])
+        return { error: `Only ${maxAvailable} are available` };
+    } else {
+
     }
-    if (maxAvailable < row[slot_count_field])
-      return { error: `Only ${maxAvailable} are available` };
   },
 };
